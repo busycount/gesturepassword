a85c43f3319e379d082ece80468fa745a6f3c87d
Count <864514196@qq.com>
2018-12-05 10:50:21 +0800

    接口适配

--- library/src/main/java/com/busycount/gesturepassword/OnGesturePasswordListener.java
+++ library/src/main/java/com/busycount/gesturepassword/OnGesturePasswordListener.java
@@ -6,4 +6,33 @@ package com.busycount.gesturepassword;
  * 2018/12/5 | Count.C | Created
  */
 public abstract class OnGesturePasswordListener implements IOnGesturePasswordListener {
+    @Override
+    public void onDrawing() {
+
+    }
+
+    @Override
+    public String readPassword() {
+        return null;
+    }
+
+    @Override
+    public void onCreateSuccess() {
+
+    }
+
+    @Override
+    public void onInvalid() {
+
+    }
+
+    @Override
+    public void onVerifySuccess(String password) {
+
+    }
+
+    @Override
+    public void onVerifyFailed() {
+
+    }
 }
